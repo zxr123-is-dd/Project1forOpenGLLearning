ba679319855cd4cc090cb005beedf1e82d409606
zxr123-is-dd <1969638695@qq.com>
2025-11-18 10:40:57 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,8 @@
 #include "Shader.h"
 #include "Model.h"
 
-constexpr unsigned int screenWidth = 1280;
-constexpr unsigned int screenHeight = 960;
+constexpr unsigned int screenWidth = 1920;
+constexpr unsigned int screenHeight = 1080;
 
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
